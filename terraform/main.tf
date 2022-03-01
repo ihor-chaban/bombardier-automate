d85f039be7bb5f95833e8a721af21dbe6be75a7e
anatolii.b <anatolii.b@opsworks.co>
2022-03-01 19:21:45 +0200

    Added terraform scripts

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh-key"
+  public_key = file("../user_rsa.pub")
+}
+
+data "aws_ami" "ubuntu" {
+  owners      = ["099720109477"]
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-*-amd64-server-*"]
+  }
+
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_launch_template" "worker" {
+  name_prefix            = "worker"
+  image_id               = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.ssh_key.key_name
+  vpc_security_group_ids = [aws_security_group.ssh.id]
+  user_data              = filebase64("${path.module}/data.sh")
+}
+
+resource "aws_autoscaling_group" "asg_worker" {
+  name               = "worker_asg"
+  availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  desired_capacity   = 2
+  max_size           = 4
+  min_size           = 1
+
+  launch_template {
+    id      = aws_launch_template.worker.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_security_group" "ssh" {
+  name = "ssh_worker"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
