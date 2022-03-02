7224d94072ebc7577b04e55d8c1a276948d5c07d
anatolii.b <anatolii.b@opsworks.co>
2022-03-02 15:57:06 +0200

    Added lifecycle

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,6 +60,10 @@ resource "aws_autoscaling_group" "asg_worker" {
     id      = aws_launch_template.worker.id
     version = "$Latest"
   }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_security_group" "ssh" {
@@ -169,12 +173,12 @@ data "archive_file" "lambda_zip_file_int" {
 
 
 resource "aws_lambda_function" "refresh_lambda" {
-  filename         = "${data.archive_file.lambda_zip_file_int.output_path}"
+  filename      = data.archive_file.lambda_zip_file_int.output_path
   function_name = "lambda_refresher"
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
 
-  source_code_hash = "${data.archive_file.lambda_zip_file_int.output_base64sha256}"
+  source_code_hash = data.archive_file.lambda_zip_file_int.output_base64sha256
 
   runtime = "python3.8"
 
