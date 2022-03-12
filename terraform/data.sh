e0cfd31ed270565fd0e660de8faad298fe13965e
ihor-chaban <igor.chaban.1995@gmail.com>
2022-03-12 16:01:17 +0200

    update terraform for python

--- terraform/data.sh
+++ terraform/data.sh
@@ -1,10 +1,11 @@
-#! /bin/bash
+#!/bin/bash
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io git
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io git python3 python3-pip
 sudo usermod -aG docker ubuntu
 git clone https://github.com/ihor-chaban/bombardier-automate.git /home/ubuntu/bombardier-automate
-nohup /home/ubuntu/bombardier-automate/bombardier-automate/bombardier-automate.sh &
+pip3 install -r /home/ubuntu/bombardier-automate/bombardier-automate/requirements.txt
+nohup /home/ubuntu/bombardier-automate/bombardier-automate/bombardier-automate.py &
